e8d3f13d6db55383abb5aa52154d995e2326eeec
dthrazak <vovamilchan@gmail.com>
2019-10-15 18:35:32 +0300

    Added subscribe section and changed background

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,12 @@
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
-    if (scroll < 1){
+    if (scroll < 100){
         $('.navbar').css('background-color', 'transparent');
+        document.getElementsByTagName("nav")[0].classList.remove("navbar-light");
+        document.getElementsByTagName("nav")[0].classList.add("navbar-dark");
     }else{
-        $('.navbar').css('background-color', '#ffa734')
+        $('.navbar').css('background-color', '#ffa734');
+        document.getElementsByTagName("nav")[0].classList.remove("navbar-dark");
+        document.getElementsByTagName("nav")[0].classList.add("navbar-light");
     }
 })
